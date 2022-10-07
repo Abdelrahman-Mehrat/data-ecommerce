cb4ff3739ed0533e094411d491ae37965071fb9e
Abdelrahman-Mehrat <82546314+Abdelrahman-Mehrat@users.noreply.github.com>
2022-10-07 03:14:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const app = express();
 const products = require("./products");
 app.use(express.json());
 app.use(cors());
-// app.get("/", (req, res) => {
-//   res.send("welcome to our online shop");
-// });
+
+app.get("/", (req, res) => {
+  res.status.json({status:"success",data:products});
+});
+
 app.get("/products", (req, res) => {
   res.send(products);
 });
+
 app.get(`/homeproduct/products/:id`, (req, res) => {
   let idNum=req.params.id
   console.log(idNum);
   res.send(products[idNum-1]);
 });
-const port = process.env.port || 5000;
-app.listen(port, console.log("Welcome Abdo on port 5000"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`Running on port: ${port}`));
